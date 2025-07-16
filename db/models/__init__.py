39ecac779e462627b53471944e67118700314e8d
sonnya <sofi.kravchenko@mail.ru>
2025-07-16 15:36:40 +0300

    Modules info + more structure

---
+++ db/models/__init__.py
@@ -0,0 +1,6 @@
+'''
+Tortoise ORM-models for the blacklist app.
+
+Could be done with SQLAlchemy for more logic/session/speed control, 
+but I chose tortoise for MVP since query logic is not that complex.
+'''
